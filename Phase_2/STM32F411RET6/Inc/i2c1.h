747822d47b9fe072cbcce7fb35687b89a9dacba8
Quanhm25 <vnquan258@gmail.com>
2026-09-25 16:07:39 +0700

    Update i2c1.h

--- Phase_2/STM32F411RET6/Inc/i2c1.h
+++ Phase_2/STM32F411RET6/Inc/i2c1.h
@@ -1,1 +1,12 @@
+#ifndef I2C1_H
+#define I2C1_H
+
+#include "stm32f4xx.h"
+
+void I2C1_Init(void);
+uint8_t I2C1_WriteBytes(uint8_t addr, uint8_t *data, uint8_t len);
+uint8_t I2C1_WriteReg(uint8_t addr, uint8_t reg, uint8_t data);
+uint8_t I2C1_ReadBytes(uint8_t addr, uint8_t *buf, uint8_t len);
+
+#endif
 
