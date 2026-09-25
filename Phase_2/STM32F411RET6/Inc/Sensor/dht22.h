8afa1399a15a5cd2f0ac5f87f5b2607cf121c9e3
Quanhm25 <vnquan258@gmail.com>
2026-09-25 16:02:19 +0700

    Update dht22.h

--- Phase_2/STM32F411RET6/Inc/Sensor/dht22.h
+++ Phase_2/STM32F411RET6/Inc/Sensor/dht22.h
@@ -1,1 +1,12 @@
+#ifndef DHT22_H
+#define DHT22_H
+
+#include "stm32f4xx.h"
+
+void DHT_GPIO_Init(void);
+void DHT_TIM_Init(void);
+uint8_t DHT_StartRead(void);
+uint8_t DHT_Process(float *thermal, float *humidity);
+
+#endif
 
