dd687be28d3e2187926e218a03df7fff50da5e5c
Quanhm25 <vnquan258@gmail.com>
2026-09-25 16:01:58 +0700

    Update bh1750.h

--- Phase_2/STM32F411RET6/Inc/Sensor/bh1750.h
+++ Phase_2/STM32F411RET6/Inc/Sensor/bh1750.h
@@ -1,1 +1,15 @@
+#ifndef BH1750_H
+#define BH1750_H
+
+#include "stm32f4xx.h"
+
+#define BH1750_ADDR 0x23
+#define BH1750_CMD_POWER_ON 0x01
+#define BH1750_CMD_RESET 0x07
+#define BH1750_CMD_CONT_H 0x10
+
+void BH1750_Init(void);
+uint8_t BH1750_ReadLux(float *lux);
+
+#endif
 
