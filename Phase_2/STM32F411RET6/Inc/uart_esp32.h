7cbe61f18d854e0a5f0144594f56fa5349d37db6
Quanhm25 <vnquan258@gmail.com>
2026-09-26 22:47:53 +0700

    Update uart_esp32.h

--- Phase_2/STM32F411RET6/Inc/uart_esp32.h
+++ Phase_2/STM32F411RET6/Inc/uart_esp32.h
@@ -10,4 +10,24 @@ extern volatile uint32_t lastCommandTime;
 
 void UART_ESP32_Init(void);
 
+typedef struct {
+	float temperature;
+	float humidity;
+	float smoke_ppm;
+	float light_lux;
+} SensorData_t;
+
+#pragma pack(push,1)
+typedef struct {
+	uint8_t start;
+	float temperature;
+	float humidity;
+	float smoke_ppm;
+	float light_lux;
+	uint8_t crc;
+	uint8_t end;
+} SensorDataFrame_t;
+
+void UART_ESP32_SendSensorData(const SensorData_t *data);
+
 #endif
